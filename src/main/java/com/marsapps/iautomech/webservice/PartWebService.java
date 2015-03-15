af0f69a0db6e40333da60c93cd430b93977e99b2
Mars <coderxster@gmail.com>
2015-03-16 10:45:59 +1100

    cleaning up dev environment

---
+++ src/main/java/com/marsapps/iautomech/webservice/PartWebService.java
@@ -0,0 +1,11 @@
+package com.marsapps.iautomech.webservice;
+
+import javax.jws.WebService;
+
+@WebService
+public class PartWebService {
+
+	public String getString() {
+		return "Here you go!";
+	}
+}
